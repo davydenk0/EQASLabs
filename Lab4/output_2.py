147f0b162cc67b7e433ec7ab0c830315380e446c
Davydenko Pavlo <davydenko.pavlo@gmail.com>
2024-05-08 08:23:53 +0300

    initial commit

---
+++ Lab4/output_2.py
@@ -0,0 +1,24 @@
+class Shape:
+    def area(self):
+        pass
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side ** 2
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius ** 2
+
+
+square = Square(5)
+print("Area of square:", square.area())
+
+circle = Circle(3)
+print("Area of circle:", circle.area())
